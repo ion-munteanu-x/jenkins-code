cbec2211032332f905694a50e344e2dfb536c8e4
raresociopath <imunteanu@codefactorygroup.com>
2019-11-24 16:09:22 +0200

    change group

---
+++ vars/toPrettyJson.groovy
@@ -0,0 +1,23 @@
+#!groovy
+
+import static groovy.json.JsonOutput.prettyPrint
+import static groovy.json.JsonOutput.toJson
+
+/**
+ * Converts the given object to pretty printed JSON.
+ *
+ * @param object, to be converted
+ *
+ * @return pretty printed JSON version of the object
+ *
+ * Example call:
+ *
+ * String ppJson = toPrettyJson(anyObject)
+ */
+String call(def object) {
+
+  // ensure that all mandatory config parameters are set
+  checkParam(object?.toString(), 'The \'object\' to be converted to pretty printed JSON must be set.')
+
+  prettyPrint(toJson(object))
+}
