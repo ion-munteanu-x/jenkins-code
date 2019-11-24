95e13a918083b77f2d793999ca8dedfa7e9cb87e
raresociopath <imunteanu@codefactorygroup.com>
2019-11-24 16:27:33 +0200

    change default rs version

--- src/com/raresociopath/jenkins/models/ProductRepositoriesData.groovy
+++ src/com/raresociopath/jenkins/models/ProductRepositoriesData.groovy
@@ -5,5 +5,5 @@ import com.raresociopath.jenkins.jobs.JobsData
 class ProductRepositoriesData {
     private def Jobs = new JobsData()
 
-    public def JenkinsJobs = new ProductRepository('JenkinsJobs', 'rs-jenkins-jobs', System.getenv('GLOBAL_DSL_DEFAULT_VERSION') ?: 'develop')
+    public def JenkinsJobs = new ProductRepository('JenkinsJobs', 'rs-jenkins-jobs', System.getenv('GLOBAL_DSL_DEFAULT_VERSION') ?: 'master')
 }
