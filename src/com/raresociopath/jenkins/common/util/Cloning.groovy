364e7ecc10021c087ea8205180d5f0f7896177c5
Ion Munteanu (CodeFactory) <ion.munteanu@codefactorygroup.com>
2019-11-25 15:01:01 +0200

    add dsl

--- src/com/raresociopath/jenkins/common/util/Cloning.groovy
+++ src/com/raresociopath/jenkins/common/util/Cloning.groovy
@@ -1,6 +1,6 @@
 package com.raresociopath.jenkins.common.dsl
 
-import com.raresociopath.jenkins.common.models.Repository
+import com.raresociopath.jenkins.common.models.RSRepository
 
 class Cloning {
 
@@ -14,7 +14,7 @@ class Cloning {
         this.logger = envvars.get('out') as PrintStream
     }
 
-    String cloneUrl(Repository repo) {
+    String cloneUrl(RSRepository repo) {
         String cloneMethod = envvars.get('SCM_CLONE_METHOD').toLowerCase()
         if (!(cloneMethod == 'ssh' || cloneMethod == 'https')) {
             logger.println("ERROR: clone method not set as SCM_CLONE_METHOD variable, fallback to $defaultCloneMethod")
