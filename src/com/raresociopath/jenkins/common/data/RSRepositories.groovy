4e54510d21f70b62dab23fe223d568ec2e45f7e3
Ion Munteanu (CodeFactory) <ion.munteanu@codefactorygroup.com>
2019-11-25 15:09:05 +0200

    add dsl

--- src/com/raresociopath/jenkins/common/data/RSRepositories.groovy
+++ src/com/raresociopath/jenkins/common/data/RSRepositories.groovy
@@ -1,16 +1,14 @@
-package com.williamhill.jenkins.common.data
+package com.raresociopath.jenkins.common.data
 
-import com.williamhill.jenkins.common.data.RSConstants
-import com.williamhill.jenkins.common.data.RSJobs
-import com.williamhill.jenkins.common.models.RSRepository
+import com.rarespciopath.jenkins.common.models.RSRepository
 
 
 class RSRepositories {
-    private RSConstants Cnst = new RSConstants()
-    private RSJobs Jobs = new RSJobs()
+    private Cnst = new RSConstants()
+    private Jobs = new RSJobs()
     private Map defaults = ['namespace': 'raresociopath', library: false]
 
-    public def RSRepository JenkinsJobs = new RSRepository(defaults + [
+    public def JenkinsJobs = new RSRepository(defaults + [
             id       : 'rs-jenkins-jobs',
             humanName: 'JenkinsJobs',
     ])
