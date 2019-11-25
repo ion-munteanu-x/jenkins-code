49362c5a11e15a079bc98403334a47095549e61f
Ion Munteanu (CodeFactory) <ion.munteanu@codefactorygroup.com>
2019-11-25 15:34:53 +0200

    add dsl

--- src/com/raresociopath/jenkins/common/data/RS.groovy
+++ src/com/raresociopath/jenkins/common/data/RS.groovy
@@ -3,5 +3,5 @@ package com.raresociopath.jenkins.common.data
 class RS {
     public static RSJobs Jobs = new RSJobs()
     public static RSConstants Constants = new RSConstants()
-    public static RSRepostitories Repos = new RSRepostitories()
+    public static RSRepositories Repos = new RSRepositories()
 }
