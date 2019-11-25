5638b0bc4a59cd00abdb2b98927493cd5397bec7
Ion Munteanu (CodeFactory) <ion.munteanu@codefactorygroup.com>
2019-11-25 16:23:01 +0200

    add dsl

--- jobs/build/BuildServicesJobs.groovy
+++ jobs/build/BuildServicesJobs.groovy
@@ -1,12 +1,12 @@
 import com.raresociopath.jenkins.rs.dsl.DSL
-import com.raresociopath.jenkins.rs.dsl.DSLParams
+import com.raresociopath.jenkins.wh.dsl.WHParams
  
 import static com.raresociopath.jenkins.wh.data.WH.Jobs
  
 pipelineJob(Jobs.HelloWorld) {
     displayName("Build Hello World")
     parameters {
-        def Params = DSLParams.get(delegate) 
+        def Params = WHParams.get(delegate) 
         Params("String", "", "String")           
         Params.dsl()
     }
