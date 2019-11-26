15a220e230a4fcfd1b68f131175230d876faf2f5
Ion Munteanu (CodeFactory) <ion.munteanu@codefactorygroup.com>
2019-11-26 14:24:12 +0200

    change names

---
+++ vars/getDefaultAppConfig.groovy
@@ -0,0 +1,12 @@
+#!groovy
+
+Map call() {
+  Map result = [:]
+  result.branch = getGitBranch()
+  result.commit = getGitCommit()
+  result.commitTags = getGitCommitTags()
+  result.commitRef = result.commit.take(8)
+  result.isDetachedHead = isDetachedHead()
+  result.normalizedBranch = result.branch.replaceAll("[_ /\\&]", "-")
+  result
+}
---
+++ vars/isDetachedHead.groovy
@@ -0,0 +1,8 @@
+#!groovy
+
+boolean call() {
+  echo "Checking if current checkouted revision is a detached HEAD."
+  def magic = "(detached head and some magic string here just to ensure this function working well)"
+  def checkResult = getShOutput("git describe --all HEAD 2>/dev/null || echo \"$magic\"")
+  checkResult == magic
+}
