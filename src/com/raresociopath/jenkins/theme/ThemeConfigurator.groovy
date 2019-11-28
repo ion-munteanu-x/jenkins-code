eee56d0f3dbaa762880fdc7eb56df36d267ca01b
Ion Munteanu (CodeFactory) <ion.munteanu@codefactorygroup.com>
2019-11-28 16:56:00 +0200

    add theme configurator

---
+++ src/com/raresociopath/jenkins/theme/ThemeConfigurator.groovy
@@ -0,0 +1,25 @@
+package com.raresociopath.jenkins.theme
+/*
+ * This script configures the simple theme plugin.
+ * Requires the simple theme plugin to be installed.
+ * Tested with simple-theme-plugin:0.5.1
+ *
+ * Use http://afonsof.com/jenkins-material-theme/ to generate a new jenkins theme.
+ * Place the theme at the userContent directory of Jenkins to be publicly available
+ */
+
+import jenkins.model.Jenkins
+import org.jenkinsci.plugins.simpletheme.CssUrlThemeElement
+
+class ThemeConfigurator {
+
+    def themeDecorator = Jenkins.instance.getExtensionList(org.codefirst.SimpleThemeDecorator.class).first()
+
+    static def configure() {
+        themeDecorator.setElements([
+        new CssUrlThemeElement('https://cdn.rawgit.com/afonsof/jenkins-material-theme/gh-pages/dist/material-blue.css')
+        ])
+
+        Jenkins.instance.save()
+    }
+}
