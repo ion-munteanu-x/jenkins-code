180e16ad610af7aa4d77db064661b479ed1e66f7
Ion Munteanu (CodeFactory) <ion.munteanu@codefactorygroup.com>
2019-11-29 17:23:22 +0200

    seed jobs

--- jobs/a_internal/SeedJobs.groovy
+++ jobs/a_internal/SeedJobs.groovy
@@ -4,7 +4,7 @@ import com.raresociopath.jenkins.dsl.DSLParams
 import static com.raresociopath.jenkins.data.StaticData.Jobs
 
 pipelineJob(Jobs.SeedCustom) {
-    displayName("Seed from a custom branch")
+    displayName("Seed Jobs")
     logRotator(7, 500)    
     parameters {
         def Params = DSLParams.get(delegate)            
