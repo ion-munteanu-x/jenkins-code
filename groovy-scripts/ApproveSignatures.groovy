acd916f517a9751bc9fb0da50949a408894c62a4
Ion Munteanu (CodeFactory) <ion.munteanu@codefactorygroup.com>
2019-12-05 13:09:19 +0200

    add approve

--- groovy-scripts/ApproveSignatures.groovy
+++ groovy-scripts/ApproveSignatures.groovy
@@ -3,7 +3,7 @@ import jenkins.model.Jenkins
 def signatures = [
         'staticMethod jenkins.model.Jenkins getInstance',
         'method jenkins.model.Jenkins getExtensionList java.lang.Class',
-        'method io.jenkins.plugins.casc.ConfigurationAsCode configure'
+        'method io.jenkins.plugins.casc.ConfigurationAsCode configure',
         'test',
 ]
 def scriptApproval = Jenkins.get().getExtensionList("org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval")[0]
