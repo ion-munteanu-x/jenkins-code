fee4a2c9e4d38fe4a0e1cae530747270e8cb198f
Ion Negara <ion.negara@codefactorygroup.com@LP20030.codefactorygroup.com>
2019-12-16 15:49:42 +0200

    added test job

---
+++ jobs/test/BuildSimpleProjectTest.groovy
@@ -0,0 +1,19 @@
+import com.raresociopath.jenkins.dsl.DSL
+import com.raresociopath.jenkins.dsl.DSLParams
+import com.raresociopath.jenkins.data.GlobalVars
+import com.raresociopath.jenkins.data.Repositories
+import com.raresociopath.jenkins.util.Cloning
+
+import static com.raresociopath.jenkins.data.StaticData.Jobs
+
+pipelineJob(proj.distJobId) {
+        displayName("Build TEST")
+        description("This is a test job")
+        logRotator(7, 50)
+        parameters {
+            def Param = DSLParams.get(delegate) 
+            Param('test_string', '', 'this string will be printed')
+            Param.dslVersion()
+        }
+        new DSL(this).pipeline(delegate, 'test/BuildSimpleProjectTest', '${Dsl_Version}')
+    }
