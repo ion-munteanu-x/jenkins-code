9e48fe870b6af6b538b76a5cc97c403b5b682c93
Ion Negara <ion.negara@codefactorygroup.com@LP20030.codefactorygroup.com>
2019-12-17 14:06:58 +0200

    new job added

---
+++ jobs/downstream_jobs/DownstreamJobs.groovy
@@ -0,0 +1,32 @@
+import com.raresociopath.jenkins.dsl.DSL
+import com.raresociopath.jenkins.dsl.DSLParams
+import com.raresociopath.jenkins.data.GlobalVars
+import com.raresociopath.jenkins.data.Repositories
+import com.raresociopath.jenkins.util.Cloning
+
+import static com.raresociopath.jenkins.data.StaticData.Jobs
+
+pipelineJob(Jobs.DownstreamJob1) {
+        displayName("FirstJob")
+        description("This is first job")
+        logRotator(7, 50)
+        parameters {
+            def Param = DSLParams.get(delegate) 
+            Param('test_string', '', 'this string will be printed')
+            Param.dslVersion()
+        }
+        new DSL(this).pipeline(delegate, 'Downstream/FirstJob', '${Dsl_Version}')
+    }
+
+pipelineJob(Jobs.DownstreamJob2) {
+        displayName("DelegatedJob")
+        description("Delegated job")
+        logRotator(7, 50)
+        parameters {
+            def Param = DSLParams.get(delegate) 
+            Param('delegated_string', '', 'this string is from another job')
+            Param('other_string', '', 'this string will be printed')
+            Param.dslVersion()
+        }
+        new DSL(this).pipeline(delegate, 'Downstream/SecondJob', '${Dsl_Version}')
+    }
--- src/com/raresociopath/jenkins/data/Jobs.groovy
+++ src/com/raresociopath/jenkins/data/Jobs.groovy
@@ -6,6 +6,7 @@ class Jobs {
     public String InternalJobsFolder = "internal"
     public String BuildJobsFolder = "build"
     public String TestJobsFolder = "test"
+    public String DownstreamJobsFolder = "Downstream"
     public String BuildSimpleProjectsFolder = "$BuildJobsFolder/build-simple-projects-folder"
 
     //jobs
@@ -19,5 +20,7 @@ class Jobs {
 
     //test
     public String TestJob = "$TestJobsFolder/test"
+    public String DownstreamJob1 = "$DownstreamJobsFolder/DownstreamJob1"
+    public String DownstreamJob2 = "$DownstreamJobsFolder/DownstreamJob2"
 
 }
